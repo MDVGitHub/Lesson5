623a486a3fcd95667a690c4a4a3320e9dacff639
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-28 21:17:20 +0300

    dz5_1

--- dz5_1/Program.cs
+++ dz5_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] GenerateRandomArray(int length,int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random(111, 999).Next(startPoint, endPoint+1);
+    }
+    return resultArray;
+}
+
+int printArray (int[] array)
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    
+    Console.Write(Array[i])+",";
+}
+
+int i[] GenerateRandomArray()
