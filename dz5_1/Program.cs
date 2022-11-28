1c303b32224020b33241b5b254ad0575d8e1186a
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-28 22:44:52 +0300

    dz5_1

--- dz5_1/Program.cs
+++ dz5_1/Program.cs
@@ -6,17 +6,24 @@ int[] GenerateRandomArray(int length,int startPoint, int endPoint)
     int[] resultArray = new int[length];
     for (int i = 0; i < length; i++)
     {
-        resultArray[i] = new Random(111, 999).Next(startPoint, endPoint+1);
+        resultArray[i] = new Random().Next(startPoint, endPoint+1);
     }
     return resultArray;
 }
 
-int printArray (int[] array)
-Console.Write("[");
-for (int i = 0; i < array.Length; i++)
+void printArray(int[] array)
 {
-    
-    Console.Write(Array[i])+",";
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
 }
 
-//int i[] GenerateRandomArray()
+int []Array = GenerateRandomArray(5, 111, 999);
+printArray(Array);
