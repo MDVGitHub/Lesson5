37bf56eb51763e5267d11799f141fec33e10edef
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-29 00:52:40 +0300

    dz5All

--- dz5_3/Program.cs
+++ dz5_3/Program.cs
@@ -11,20 +11,39 @@ double[] GenerateRandomArray(int length,int startPoint, int endPoint)
     return resultArray;
 }
 
-int getDiffMinOfMaxElementInArray(int[] array)
+double MaxElementArray (double [] array)
 {
-    int sum = 0;
+    double max = array[0];
     for (int i = 0; i < array.Length; i++)
     {
-        if ((i % 2 ) > 0)
+        if(array[i] > max)
         {
-            sum += array[i];
+            max = array[i];
         }
     }
-    return sum;
+    return max;
 }
 
-void printArray(int[] array)
+double MinElementArray (double [] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double getDiffMinOfMaxElementInArray(double max, double min)
+{
+    double diff = max - min;
+    return diff;
+}
+
+void printArray(double[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
@@ -38,8 +57,11 @@ void printArray(int[] array)
     Console.WriteLine("]");
 }
 
-int []Array = GenerateRandomArray(4, -99, 99);
+double []Array = GenerateRandomArray(4, 1, 10);
 printArray(Array);
 Console.WriteLine();
-int DiffOfElements = getDiffMinOfMaxElementInArray(Array);
+
+double Maxx = MaxElementArray (Array);
+double Min = MinElementArray (Array);
+double DiffOfElements = getDiffMinOfMaxElementInArray(Maxx, Min);
 Console.WriteLine($"Разница между максимальным и минимальным элементов массива = {DiffOfElements}");
